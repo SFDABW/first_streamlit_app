468d2092ac87e6c1b3038a1b381e2b004800242f
SFDABW <131304599+SFDABW@users.noreply.github.com>
2023-05-01 15:12:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,10 +45,7 @@ my_cur.execute("Select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
-
-
-
-streamlit.write('Thanks for adding', add_my_fruit)
+streamlit.write('Thanks for adding ', add_my_fruit)
 
 #This wll not work correctly, but just go with it for now
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
